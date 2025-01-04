b100163fd9e50af20a4c455c859833a62f0843b4
andei_buite <andei.buite@outlook.com>
2025-01-04 15:44:50 +0800

    Initial Commit

---
+++ VMStart/VMStart.h
@@ -0,0 +1,18 @@
+//
+//  VMStart.h
+//  VMStart
+//
+//  Created by Andei Buite on 2025/01/04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VMStart.
+FOUNDATION_EXPORT double VMStartVersionNumber;
+
+//! Project version string for VMStart.
+FOUNDATION_EXPORT const unsigned char VMStartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VMStart/PublicHeader.h>
+
+
---
+++ VMStartTests/VMStartTests.swift
@@ -0,0 +1,17 @@
+//
+//  VMStartTests.swift
+//  VMStartTests
+//
+//  Created by Andei Buite on 2025/01/04.
+//
+
+import Testing
+@testable import VMStart
+
+struct VMStartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
